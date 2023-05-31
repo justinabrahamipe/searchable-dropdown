29c851d397bcf6db28ce696d5946b076a1e91ce3
Justin Abraham Ipe <justinabrahamipe@gmail.com>
2023-05-31 10:48:33 +0100

    linting done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './boilerplate-code/index.css';
+import App from './boilerplate-code/App';
+import reportWebVitals from './boilerplate-code/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
